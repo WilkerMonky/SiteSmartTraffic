34b464859f60ccf49ee6974e76ef697200eeb908
Wilker <weslleywilker2@gmail.com>
2023-06-20 23:42:04 -0300

    Mudanças nos servicos, adição das models, adição da MEDIA_ROOT

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Membro, Cargo, Registro
 
-# Register your models here.
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo')
+
+
+@admin.register(Membro)
+class MembroAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo')
+
+@admin.register(Registro)
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'etapa', 'objetivo', 'ativo')
