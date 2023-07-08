72724bdca478c312d38f11c0748c567b1c01a20b
Wilker <weslleywilker2@gmail.com>
2023-07-07 22:05:11 -0300

    ss

--- semaforo/settings.py
+++ semaforo/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
